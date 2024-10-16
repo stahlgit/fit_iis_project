4e2c6c91cc5309cf699c6ca9bf03abbdc6915248
Peter Stahl <peter.stahl@betrian.cz>
2024-10-16 14:32:57 +0200

    ADD: models

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,17 @@ from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 from app.api.base import Base
+from app.api.models import (
+    Conference,
+    GivenPresentation,
+    Lecture,
+    Question,
+    Reservation,
+    Room,
+    Ticket,
+    User,
+    Voting,
+)
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -37,13 +48,14 @@ def get_url():
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
+        This configures the context with just a URL
+        and not an Engine, though an Engine is acceptable
+        here as well.  By skipping the Engine creation
+        we don't even ne    url = get_url()
+    ed a DBAPI to be available.
 
-    Calls to context.execute() here emit the given string to the
-    script output.
+        Calls to context.execute() here emit the given string to the
+        script output.
 
     """
     url = get_url()
--- app/api/models.py
+++ app/api/models.py
@@ -1,4 +1,5 @@
-from typing import List
+from decimal import Decimal
+from typing import List, Optional
 
 from sqlalchemy import (
     Boolean,
@@ -8,7 +9,10 @@ from sqlalchemy import (
     ForeignKey,
     Integer,
     String,
+    Text,
+    func,
 )
+from sqlalchemy.dialects.postgresql import TSTZRANGE
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.api.base import (
@@ -24,53 +28,59 @@ class Conference(BaseModelMixin, Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
-    description: Mapped[str] = mapped_column(String, nullable=False)
-    genre: Mapped[str] = mapped_column(String, nullable=False)
-    place: Mapped[str] = mapped_column(String, nullable=False)
-    time_from = mapped_column(DateTime, nullable=False)
-    time_to = mapped_column(DateTime, nullable=False)
-    price = mapped_column(Float, nullable=False)
-    capacity = mapped_column(Integer, nullable=False)
+
+    description: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
+    genre: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    place: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    time_interval: Mapped[Optional[TSTZRANGE]] = mapped_column(
+        "time_interval", TSTZRANGE, nullable=False
+    )
+    # TIME TO // TIME FROM ?
+    price: Mapped[Optional[Float]] = mapped_column(Float, nullable=True)
+    capacity: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
 
     rooms: Mapped[List["Room"]] = relationship("Room", back_populates="conference")
 
     lectures: Mapped[List["Lecture"]] = relationship(
-        "Lecture", back_populates="conference"
+        "Lecture", back_populates="conference", cascade="all, delete, delete-orphan"
     )
 
-    reservation = Mapped[List["Reservation"]] = relationship(
+    reservation: Mapped[List["Reservation"]] = relationship(
         "Reservation", back_populates="conference", cascade="all, delete, delete-orphan"
     )
-    given_presentations = Mapped[List["GivenPresentation"]] = relationship(
-        "GivenPresentation", back_populates="conference"
-    )
 
-    __table_args__ = (
-        CheckConstraint("time_from < time_to", name="Correct_time_sequence"),
+    given_presentations: Mapped[List["GivenPresentation"]] = relationship(
+        "GivenPresentation",
+        back_populates="conference"
+        ## track history ???
     )
 
 
 class User(BaseModelMixin, Base):
     __tablename__ = "users"
 
-    id = mapped_column(Integer, primary_key=True)
-    name = mapped_column(String, nullable=False)
-    email = mapped_column(String, nullable=False)
-    role = mapped_column(String, nullable=False)  ##TODO Enum ???
-    ## TODO Add password
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False)
+    role: Mapped[Optional[str]] = mapped_column(String, nullable=False)  ##TODO Enum ???
+    # TODO password hashing
 
-    lectures = Mapped[List["Lecture"]] = relationship(
-        "Lecture", back_populates="lecturer"
+    lectures: Mapped[List["Lecture"]] = relationship(
+        "Lecture",
+        back_populates="lecturer"
+        # no histoty to annonymize
     )
 
-    votings = Mapped[List["Voting"]] = relationship("Voting", back_populates="user")
-    questions = Mapped[List["Question"]] = relationship(
+    votings: Mapped[List["Voting"]] = relationship("Voting", back_populates="user")
+
+    questions: Mapped[List["Question"]] = relationship(
         "Question", back_populates="user"
     )
-    reservations = Mapped[List["Reservation"]] = relationship(
+
+    reservations: Mapped[List["Reservation"]] = relationship(
         "Reservation", back_populates="user", cascade="all, delete, delete-orphan"
     )
-    given_presentations = Mapped[List["GivenPresentation"]] = relationship(
+    given_presentations: Mapped[List["GivenPresentation"]] = relationship(
         "GivenPresentation", back_populates="user"
     )
 
@@ -80,42 +90,57 @@ class Room(BaseModelMixin, Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
-    capacity = mapped_column(Integer, nullable=False)
-    conference_id = mapped_column(Integer, ForeignKey("conferences.id"), nullable=False)
+    capacity: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+    conference_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("conferences.id"), nullable=True
+    )
 
     lectures: Mapped[List["Lecture"]] = relationship("Lecture", back_populates="room")
 
-    conference = relationship("Conference", back_populates="rooms")
+    conference: Mapped["Conference"] = relationship(
+        "Conference", back_populates="rooms"
+    )
 
 
 class Reservation(BaseModelMixin, Base):
     __tablename__ = "reservations"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    number_of_tickets = mapped_column(Integer, nullable=False)
-    status = mapped_column(String, nullable=False)
-    paid = mapped_column(Boolean, nullable=False)
+    number_of_tickets: Mapped[int] = mapped_column(Integer, nullable=False)
+    status: Mapped[str] = mapped_column(String, nullable=True)
+    paid: Mapped[bool] = mapped_column(Boolean, nullable=False)
 
-    user_id = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
-    conference_id = mapped_column(Integer, ForeignKey("conferences.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("users.id"), nullable=False
+    )
+    conference_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("conferences.id"), nullable=False
+    )
 
-    user = relationship("User", back_populates="reservations")
-    conference = relationship("Conference", back_populates="reservations")
+    user: Mapped["User"] = relationship("User", back_populates="reservations")
+    conference: Mapped["Conference"] = relationship(
+        "Conference", back_populates="reservations"
+    )
+    tickets: Mapped[List["Ticket"]] = relationship(
+        "Ticket", back_populates="reservation", cascade="all, delete, delete-orphan"
+    )
 
 
 class Ticket(BaseModelMixin, Base):
     __tablename__ = "tickets"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    conference_id = mapped_column(Integer, nullable=False)
-    time_created = mapped_column(DateTime, nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now()
+    )
     ## HOW ABOUT ??? price = mapped_column(Float, nullable=False)
 
-    reservation_id = mapped_column(
-        Integer, ForeignKey("reservations.id"), nullable=False
+    reservation: Mapped["Reservation"] = relationship(
+        "Reservation", back_populates="tickets"
     )
-
-    reservation = relationship("Reservation", back_populates="tickets")
+    reservation_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("reservations.id", ondelete="CASCADE"), nullable=False
+    )
 
 
 class Lecture(BaseModelMixin, Base):
@@ -123,28 +148,32 @@ class Lecture(BaseModelMixin, Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
-    description: Mapped[str] = mapped_column(String, nullable=False)
-    time_from = mapped_column(DateTime, nullable=False)
-    time_to = mapped_column(DateTime, nullable=False)
-    tags = mapped_column(String, nullable=False)
-    image = mapped_column(String, nullable=False)
+    description: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    time_interval: Mapped[TSTZRANGE] = mapped_column(TSTZRANGE, nullable=True)
 
-    room_id = mapped_column(Integer, ForeignKey("rooms.id"), nullable=False)
-    conference_id = mapped_column(Integer, ForeignKey("conferences.id"), nullable=False)
-    lecturer_id = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
+    tags: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    image: Mapped[Optional[str]] = mapped_column(String, nullable=True)
 
-    room = relationship("Room", back_populates="lectures")
-    conference = relationship("Conference", back_populates="lectures")
-    lecturer = relationship("User", back_populates="lectures")
-
-    votings = Mapped[List["Voting"]] = relationship("Voting", back_populates="lecture")
-
-    questions = Mapped[List["Question"]] = relationship(
-        "Question", back_populates="lecture"
+    room_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("rooms.id", ondelete="CASCADE"), nullable=False
+    )
+    conference_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("conferences.id", ondelete="CASCADE"), nullable=False
+    )
+    lecturer_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
     )
 
-    __table_args__ = (
-        CheckConstraint("time_from < time_to", name="Correct_time_sequence"),
+    room: Mapped["Room"] = relationship("Room", back_populates="lectures")
+    conference: Mapped["Conference"] = relationship(
+        "Conference", back_populates="lectures"
+    )
+    lecturer: Mapped["User"] = relationship("User", back_populates="lectures")
+
+    votings: Mapped[List["Voting"]] = relationship("Voting", back_populates="lecture")
+
+    questions: Mapped[List["Question"]] = relationship(
+        "Question", back_populates="lecture", cascade="all, delete, delete-orphan"
     )
 
 
@@ -152,28 +181,54 @@ class GivenPresentation(BaseModelMixin, Base):
     __tablename__ = "given_presentations"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    """ user_id = mapped_column(Integer, nullable=False)
-    lecture_id = mapped_column(Integer, nullable=False) """
-    design = mapped_column(String, nullable=False)
-    status = mapped_column(String, nullable=False)
+    user_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("users.id"), nullable=False
+    )
+    conference_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("conferences.id"), nullable=False
+    )
+    proposal: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    status: Mapped[str] = mapped_column(String, nullable=False)
 
-    user = relationship("User", back_populates="given_presentations")
-    conference = relationship("Conference", back_populates="given_presentations")
+    user: Mapped["User"] = relationship("User", back_populates="given_presentations")
+    conference: Mapped["Conference"] = relationship(
+        "Conference", back_populates="given_presentations"
+    )
 
 
 class Voting(BaseModelMixin, Base):
     __tablename__ = "voting"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    user = relationship("User", back_populates="votings")
-    lecture = relationship("Lecture", back_populates="votings")
+
+    user_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("users.id"), nullable=False
+    )
+    lecture_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("lectures.id"), nullable=False
+    )
+    user: Mapped["User"] = relationship("User", back_populates="votings")
+    lecture: Mapped["Lecture"] = relationship("Lecture", back_populates="votings")
+
+    rating: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    __table_args__ = (
+        CheckConstraint("rating >= 1 AND rating <= 5", name="rating_check"),
+    )
 
 
 class Question(BaseModelMixin, Base):
     __tablename__ = "questions"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    text: Mapped[str] = mapped_column(String, nullable=False)
+    text: Mapped[str] = mapped_column(Text, nullable=False)
 
-    user = relationship("User", back_populates="questions")
-    lecture = relationship("Lecture", back_populates="questions")
+    user_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("users.id"), nullable=False
+    )
+    lecture_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("lectures.id"), nullable=False
+    )
+
+    user: Mapped["User"] = relationship("User", back_populates="questions")
+    lecture: Mapped["Lecture"] = relationship("Lecture", back_populates="questions")
