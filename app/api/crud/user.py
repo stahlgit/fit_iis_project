58062fb87c910b8c315e1d8f2878224cdebcbedd
Peter Stahl <peter.stahl@betrian.cz>
2024-11-12 11:15:05 +0100

    ADD: authenticate_user error handling

--- app/api/crud/user.py
+++ app/api/crud/user.py
@@ -25,12 +25,19 @@ async def authenticate_user(
     password: str,
     db: Session = Depends(get_db),
 ) -> Optional[User]:
-    user = User.get_by(db, name=username)
-    ## password je input od usera
-    ## hashed password je z DB
-    if not user or not verify_password(password, user.hashed_password):
-        return False
-    return user
+    try:
+        user = User.get_by(db, name=username)
+        ## password je input od usera
+        ## hashed password je z DB
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+        if not verify_password(password, user.hashed_password):
+            raise HTTPException(status_code=400, detail="Incorrect password")
+        return user
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error occured: {e}")
 
 
 async def create_access_token(data: dict) -> str:
