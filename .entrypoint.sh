01b81a205a1ae699a17f254a58441d7bda7c1c6a
Robert Blažek <35804312+robertblazek@users.noreply.github.com>
2024-11-16 20:34:37 +0100

    debug: .entrypoint.sh debug messages, copy docker-compose-deploy.yml and explicitly specify enviroment variables

--- .entrypoint.sh
+++ .entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+echo "starting ..."
+echo "nslookup ..."
+nslookup iis_database
+
+env
+
 set -e
 
+echo "alembic upgrade ..."
+
 poetry run alembic upgrade head
+
+echo "api starting ..."
 
 exec uvicorn main:app --host 0.0.0.0 --port 8000 --reload --workers 1
