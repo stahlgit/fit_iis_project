5128ce991c0ef6082a7470a65e160e9cc9d9d3f3
Peter Stahl <peter.stahl@betrian.cz>
2024-11-22 15:20:23 +0100

    ADD: DEV CORS enviroment

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import importlib
 import os
 
+from dotenv import load_dotenv
 from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
+
+load_dotenv()
 
 app = FastAPI(
     title="FIT ISS PROJECT API",
     version="0.1",
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # allow all origins (for development purposes)
-    # later allow_origins=["http://localhost:3000"] for frontend
-    allow_credentials=True,
-    allow_methods=["*"],  # allow all methods (GET, POST, PUT, DELETE, etc.)
-    allow_headers=["*"],  # allow all headers
-)
+
+if os.getenv("DEV") == "true":
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # allow all origins (for development purposes)
+        # later allow_origins=["http://localhost:3000"] for frontend
+        allow_credentials=True,
+        allow_methods=["*"],  # allow all methods (GET, POST, PUT, DELETE, etc.)
+        allow_headers=["*"],  # allow all headers
+    )
 
 Instrumentator().instrument(app).expose(app)
 
